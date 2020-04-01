706d1fd450eaa4967a8d81e25d3fd658b97737b3
EviyEmoji <43426819+EviyEmoji@users.noreply.github.com>
2020-04-01 15:02:11 -0700

    decent ui

--- src/main/java/com/oreizy/everest/planner/Main.java
+++ src/main/java/com/oreizy/everest/planner/Main.java
@@ -16,34 +16,77 @@ import spark.template.freemarker.FreeMarkerEngine;
  * @author eoreizy
  */
 public class Main {
+
     public static void main(String[] args) {
-        port(4567);
-        
+        port(80);
+
         staticFiles.location("/");
-        
+
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("ip", req.ip());
-            return new FreeMarkerEngine().render(new ModelAndView(model, "index.ftl"));
+            return new FreeMarkerEngine().render(new ModelAndView(model, "index.html"));
         });
-        
+
+        get("/string", (req, res) -> {
+            return "Hello world!";
+        });
+
         get("/internals/add/task", (req, res) -> {
             String text = req.queryParams("content");
             String listId = req.queryParams("list_id");
             String tag = req.queryParams("tag_id");
-            
+
             //do all my fancy db stuff
-            
             return "added task with " + text + " to list " + listId + " with tag " + tag;
-            
+
         });
-        
+
         get("/internals/get/tasks", (req, res) -> {
             String listId = req.queryParams("list_id");
-            
+
             //do all my db stuff
+            return new String();
+        });
+
+        get("/board/:id", (req, res) -> {
+            String boardId = req.params(":id");
+
+            //would get stuff from db instead of static arrays
+            String[] listNames = new String[]{
+                "Monday 13",
+                "Tuesday 14",
+                "Wednesday 15",
+                "Thursday 16",
+                "Friday 17"
+            };
+
+            String[] listItems = new String[]{
+                "Open NetBeans",
+                "Write some code",
+                "Have a quarantine party time",
+                "Try out what really long Strings do to my website. Will it break or survive?",
+                "Drink water - hydrate or diedrate!"
+            };
+
+            String boardTitle = "My Awesome Board";
+
             
-            return "getting tasks";
-        })
+            
+            //put it in the model and render
+            Map<String, Object> model = new HashMap<>();
+            model.put("Board_Id", boardId);
+            model.put("Board_Title", boardTitle);
+            
+            for (int i = 0; i < listNames.length; i++) {
+                model.put("List_Title_" + i, listNames[i]);
+            }
+
+            for (int i = 0; i < listItems.length; i++) {
+                model.put("Item_Text_" + i, listItems[i]);
+            }
+
+            return new FreeMarkerEngine().render(new ModelAndView(model, "board.html"));
+        });
     }
 }
--- target/classes/spark/template/freemarker/index.ftl
+++
@@ -1,2 +0,0 @@
-<h1>Welcome to planner</h1>
-You are ${ip}.
